2cf24fcf21b1409032c921af57972a61219338b9
gabimartini <gabimartinii@hotmail.com>
2024-03-13 16:27:33 +0000

    add listening page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
+const dummy_data = [
+  {
+    referenceNumber: '2024/0648/T',
+    address: "4 Netherhall Gardens London NW3 5RR", 
+    description: "FRONT GARDEN: 3 x Leylandii (T1) - Prune height & width up to 2.5m & shape. REAR GARDEN: 1 x Walnut Tree (T2) - Prune height & width up to 2m & shape. 2 x Laurels & Elaeagnus (T3) - Prune height & width up to 2.5m & shape. 1 x Sycamore (T4) - Prune height & width up to 2m & shape. 4 x Cypress (T4) - Prune height & width up to 2.5m & shape. 2 x Leylandii (T5) - Prune height up to 3m, width 1.5m & shape. 5 x Laurels (T6a & T6b) - Prune height up to 2.5m, width 1.5m & shape. 1 x Robinia (T7) - Fell to ground level. 1 x Catalpa (T8) - Prune by up to 2m all around.",
+    applicationType: "Trees",
+    date: "19-02-2024",
+    status: "registered"
+  },
+    {
+    referenceNumber: '2024/0645/T',
+    address: "4 Oak Hill Park London NW3 7LG", 
+    description: "FRONT GARDEN: 1 x Oak (T1) - Reduce lower canopy on drive side by 2m - 3m, up to 6m in height. Row of Leylandii (G1) - Reduce or remove lower branches as appropriate in order to give 5m clearance along driveway.",
+    applicationType: "Trees",
+    date: "19-02-2024",
+    status: "registered"
+  }
+]
+const tableHead = ['Reference Number', 'Address', 'Description', 'Application Type', 'Date Submited', 'Status']
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main >
+      <table>
+        <tr>
+      {
+        tableHead.map((thead, index) => (
+          <th key={index}>{thead} <button>Up</button> <button>Down</button></th>
+        ))
+      }
+      </tr>
+      {
+        dummy_data?.map((application, index : any) => (
+          <tr key={index} >
+          <td>{application.referenceNumber}</td>
+          <td>{application.address}</td>
+          <td style={{maxWidth: "40rem"}}>{application.description}</td>
+          <td>{application.applicationType}</td>
+          <td>{application.date}</td>
+          <td>{application.status}</td>
+          </tr>
+        ))
+      
+      }
+      </table>
     </main>
   );
 }
