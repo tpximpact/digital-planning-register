a698c7b8a8410e6ef3078ad94eaca01af12e464f
Madeleine Williams <maddie.williams@tpximpact.com>
2024-03-20 15:33:04 +0000

    Config map WIP needs styling

---
+++ src/components/head/index.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        src="https://cdn.jsdelivr.net/npm/@opensystemslab/map"
+        defer
+      ></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
---
+++ src/components/map/index.tsx
@@ -0,0 +1,66 @@
+import { useEffect } from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "my-map": any;
+    }
+  }
+}
+
+interface MapProps {
+  geojsonColor?: string;
+  geojsonBuffer?: string;
+  hideResetControl?: boolean;
+  // staticMode?: boolean;
+  osVectorTilesApiKey?: string;
+  geojsonData?: string;
+}
+
+const Map: React.FC<MapProps> = ({
+  geojsonColor = "#ff0000",
+  geojsonBuffer = "68",
+  hideResetControl = true,
+  // staticMode = true,
+  osVectorTilesApiKey = "",
+  geojsonData = ""
+}) => {
+  useEffect(() => {
+    const map = document.querySelector("my-map");
+    if (map) {
+      map.addEventListener("ready", (event) => {
+        console.log("map ready");
+      });
+
+      map.addEventListener("areaChange", (event: Event) => {
+        console.debug({ area: (event as CustomEvent).detail });
+      });
+
+      map.addEventListener("geojsonChange", (event: Event) => {
+        console.debug({ geojson: (event as CustomEvent).detail });
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <my-map
+        zoom="14"
+        geojsonColor={geojsonColor}
+        geojsonBuffer={geojsonBuffer}
+        hideResetControl={hideResetControl}
+        // staticMode={staticMode}
+        osVectorTilesApiKey={osVectorTilesApiKey}
+        geojsonData={geojsonData}
+      />
+      <style jsx>{`
+        my-map {
+          width: 250px;
+          height: 250px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Map;
