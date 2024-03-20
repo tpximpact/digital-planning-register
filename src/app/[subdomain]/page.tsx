51f6363f36d2099bbd00a677283ef9b69f64f05f
gabimartini <gabimartinii@hotmail.com>
2024-03-20 16:02:57 +0000

    add dummy data

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,39 +1,106 @@
-"use client";
+"use client"
+import { useEffect, useState,  } from "react";
+import { format } from "date-fns";
+import ReactPaginate from "react-paginate";
+import {getApplicationsByCouncil, getApplicationById} from "../../server";
+import Link from "next/link";
+import {NextIcon, PreviewIcon} from '../../../public/icons';
+import {Data} from "../../../util/type"
+import Form from "@/components/form";
+import { useParams } from "next/navigation";
 
-import { useParams } from "next/navigation"
-import {getApplicationsByCouncil} from "../../server/index"
-import { useEffect, useState } from "react";
-
-
-// Our types for the site data
-export interface Props {
-  name: String
-  description: String
-  subdomain: String
-}
-
+const resultsPerPage = 10
+export const tableHead = [{name: 'Reference Number', icon: true}, {name: 'Address', icon: false}, {name:'Description', icon: false}, {name: 'Application Type', icon: true}, {name:'Date Submited', icon: true}, {name:'Status', icon: true}]
 export default function Home() {
-  const [data, setData] = useState([])
   const params = useParams();
+  const [data, setData] = useState<Data[]>([])
+  const [metaData, setMetaData] = useState<any>(undefined)
+  const [idReference, setIdReference] = useState<number>(0)
+  const council = params.subdomain
 
   useEffect(() => {
-    async function callData(){
-      const council = params.subdomain;
-      const response = await getApplicationsByCouncil(council)
-      setData(response.data)
-    }
-    callData()
-  }, [params.subdomain])
+    
+    (async() => {
 
+      const response = await getApplicationsByCouncil(council, 1, resultsPerPage);
+      setData(response.data);
+      setMetaData(response.metadata)
+      console.log({response})
+    })
+    ()
+  }, [council])
 
-  return (
-    <>
-{data?.map((application: any) => (
-  <>
-    <p style={{padding: "5px", fontWeight: 'bold'}}>description: <span style={{fontWeight: 'normal'}}>{application?.description}</span></p>
-  </>
-))}
-    </>
-  )
+async function handlePageClick(event: any) {
+  const response = await getApplicationsByCouncil(council, event.selected + 1, resultsPerPage);
+      setData(response.data);
 }
 
+// in the future it should change to byReferenceNumber
+async function searchById(event: any) {
+  event.preventDefault()
+  const data = await getApplicationById(idReference, council as string)
+  setData([data] as Data[])
+  setMetaData(undefined)
+}
+  return (
+    <main style={{overflowX: 'auto'}}>
+      {
+        data?.length > 0 &&
+
+      <>
+      <Form searchById={(event: any) => searchById(event)} setIdReference={setIdReference}/>
+          <table className="landing-table-content govuk-table">
+              <tbody>
+                <tr>
+                  {tableHead.map((thead, index) => (
+                    <th key={index} className="govuk-table__head"><p>{thead.name} 
+                    {/* {thead.icon && <span><SortIcon /></span>} */}
+                    </p></th>
+                  ))}
+
+
+                </tr>
+                {data?.map((application: any, index: any) => (
+                  <tr key={index} className="govuk-table__row">
+                    <td className="govuk-table__cell"><Link href={`/${application.id}`}>{application?.reference}</Link></td>
+                    <td className="govuk-table__cell">{application?.site?.address_1}, {application?.site?.postcode}</td>
+                    <td className="govuk-table__cell" style={{ maxWidth: "40rem" }}>{application?.description}</td>
+                    <td className="govuk-table__cell">{application?.application_type.replace(/_/g, " ")}</td>
+                    <td className="govuk-table__cell">{`${format(new Date(application?.received_date), 'dd-MM-yyyy')}`}</td>
+                    <td className="govuk-table__cell">{application?.status.replace(/_/g, " ")}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <section className="pagination-section">
+              {
+                metaData?.total_pages > 1 && (
+              <>
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel={<NextIcon />}
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={4}
+                marginPagesDisplayed={1}
+                pageCount={metaData?.total_pages}
+                previousLabel={<PreviewIcon />}
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active-page"
+                renderOnZeroPageCount={null} />
+                </>
+                )
+                }
+            </section></>
+
+      }
+    </main>
+  );
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,31 +1,33 @@
 "use server"
 
-export async function getApplicationsByCouncil(council: string | string[]) {
+export async function getApplicationsByCouncil(council: string | string[], page: number, resultsPerPage: number) {
 const apiKey = council + '_api_key'
- const data = await fetch(`https://${council}.bops-staging.services/api/v2/planning_applications?page=1&maxresults=10`, {
+let data: any;
+
+if(council === 'camden') {data = await fetch(`${process.env.NEXT_PUBLIC_BOPS_API_DUMMY}`, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${process.env[apiKey]}`
+  }})
+ } else { data = await fetch(`https://${council}${process.env.NEXT_PUBLIC_BOPS_API}planning_applications?page=${page}&maxresults=${resultsPerPage}`, {
   method: 'GET',
   headers: {
     'Authorization': `Bearer ${process.env[apiKey]}`
   }
-});
+})
+ }
 
  return data.json()
-// export async function getApplicationsByCouncil(page: number, resultsPerPage: number) {
-//  const data = await fetch(`${process.env.NEXT_PUBLIC_BOPS_API}planning_applications?page=${page}&maxresults=${resultsPerPage}`, {
-//   method: 'GET',
-//   headers: {
-//     'Authorization': `Bearer ${process.env.southwark_api_key}`
-//   }
-// });
-//  return data.json()
 }
 
 // This function search by int ID, in the future it should search by reference number
-export async function getApplicationById(id:number) {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BOPS_API}planning_applications/${id}`, {
+export async function getApplicationById(id:number, council: string) {
+
+  const apiKey = council + '_api_key'
+  const data = await fetch(`https://${council}${process.env.NEXT_PUBLIC_BOPS_API}planning_applications/${id}`, {
       method: 'GET',
       headers: {
-    'Authorization': `Bearer ${process.env.southwark_api_key}`
+    'Authorization': `Bearer ${process.env[apiKey]}`
   }
   })
   return data.json()
