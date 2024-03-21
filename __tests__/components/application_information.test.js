a578a0e8b1a39deb3494bab00e880cd917163a43
Madeleine Williams <maddie.williams@tpximpact.com>
2024-03-21 15:39:23 +0000

    More tests

--- __tests__/components/application_information.test.js
+++ __tests__/components/application_information.test.js
@@ -1,25 +1,52 @@
-import ApplicationInformation from "@/components/application_information"
-import { screen, render } from "@testing-library/react"
-describe('Render ApplicationPeople', () => {
-            const mockData = {
-            reference_in_full: "AAA_BBB_CCC_DDD", 
-            application_type: "planning_application", 
-            site: {
-                address_1: "40, STANSFIELD ROAD", 
-                postcode: "SW9 9RZ"
-}, 
-            received_date: "2024-03-18T00:00:00.000+00:00", 
-            result_flag: null, 
-            determination_date: "2024-03-19", 
-            status: "not_started", 
-            consultation: {end_date: "2024-04-08"} 
-        }
+import ApplicationInformation from "@/components/application_information";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-    it('should render correct', () => {
-        render(<ApplicationInformation {...mockData}/>)
+jest.mock("../../src/components/map", () => {
+  return jest.fn(() => <div data-testid="mockMap"></div>);
+});
 
-        expect(screen.getByRole("application-reference", {Name: mockData.reference_in_full}))
-        expect(screen.getByRole("application-type", {Name: mockData.application_type}))
-        expect(screen.getByRole("application-status", {Name: "not started"}))
-    })
-})
+describe("Render ApplicationInformation", () => {
+  const mockData = {
+    reference_in_full: "AAA_BBB_CCC_DDD",
+    application_type: "planning_application",
+    site: {
+      address_1: "40, STANSFIELD ROAD",
+      postcode: "SW9 9RZ"
+    },
+    received_date: "2024-03-18T00:00:00.000+00:00",
+    result_flag: null,
+    determination_date: "2024-03-19",
+    status: "not_started",
+    consultation: { end_date: "2024-04-08" },
+    boundary_geojson: {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0, 0],
+            [1, 1],
+            [1, 0],
+            [0, 0]
+          ]
+        ]
+      }
+    }
+  };
+
+  it("should render correct", () => {
+    render(<ApplicationInformation {...mockData} />);
+    expect(
+      screen.getByRole("application-reference", {
+        Name: mockData.reference_in_full
+      })
+    );
+    expect(
+      screen.getByRole("application-type", { Name: mockData.application_type })
+    );
+    expect(screen.getByRole("application-status", { Name: "not started" }));
+    const mapComponent = screen.getByTestId("mockMap");
+    expect(mapComponent).toBeInTheDocument();
+  });
+});
