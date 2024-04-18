1676303bbb3afa8da830047d5720daf940951298
Madeleine Williams <maddie.williams@tpximpact.com>
2024-04-18 12:42:07 +0100

    document grid, conditional logic and tag formatting

--- src/components/application_files/index.tsx
+++ src/components/application_files/index.tsx
@@ -1,31 +1,61 @@
 import Image from "next/image";
-import file from "../../../public/images/file.jpg";
+import file from "../../../public/images/file-icon-default.svg";
 import { Data } from "../../../util/type";
 
 const ApplicationFile = ({ documents }: Data) => {
+  const formatTag = (tag: any) => {
+    if (tag.includes(".")) {
+      const parts = tag.split(".");
+      const lastPart = parts.pop();
+      return [lastPart, parts.join(" ")]
+        .map((part) =>
+          part
+            .split(/(?=[A-Z])/)
+            .map((word: any) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ")
+        )
+        .join(" ");
+    } else {
+      return tag
+        .split(/(?=[A-Z])/)
+        .map((part: any) => part.charAt(0).toUpperCase() + part.slice(1))
+        .join(" ");
+    }
+  };
   return (
     <>
-      {documents && documents?.length > 0 && (
+      <h2 className="govuk-heading-l">Documents</h2>
+      <p className="govuk-body documents-containers">
+        To find out more detailed information, please read the following
+        document(s) provided by the applicant.
+      </p>
+      {documents && documents.length > 0 ? (
         <>
-          <h2 className="govuk-heading-l">Files</h2>
-          <p className="govuk-body documents-containers">
-            To find out more detailed information, please read the following
-            document(s) provided by the applicant.
-          </p>
-          <div style={{ display: "flex", gap: "50px", flexWrap: "wrap" }}>
-            {documents.map((document: any, index: any) => (
-              <div key={index} style={{ display: "flex", gap: "0 15px" }}>
-                <Image src={file} alt="File" width={130} height={160} />
-                <div className="" style={{ maxWidth: "20rem" }}>
+          <div className="govuk-grid-row grid-row-extra-bottom-margin">
+            {documents.slice(0, 6).map((document: any, index: any) => (
+              <div key={index} className="govuk-grid-column-one-third">
+                <div className="govuk-grid-column-one-third">
+                  <Image
+                    src={file}
+                    alt="File"
+                    width={130}
+                    height={160}
+                    className="file-icon"
+                  />
+                </div>
+                <div className="govuk-grid-column-two-thirds">
                   <p className="govuk-body">
                     <a
                       href={document?.url}
                       className="govuk-link govuk-link--no-visited-state"
                     >
-                      {document.applicant_description}
+                      {document.tags.map(formatTag).join(", ")}
                     </a>
                   </p>
-                  <p className="govuk-hint">PDF, 8.94 MB, 62 pages</p>
+                  <p className="govuk-hint">
+                    uploaded{" "}
+                    {new Date(document.created_at).toLocaleDateString("en-GB")}
+                  </p>
                   <p className="govuk-hint">
                     This file may not be suitable for users of assistive
                     technology.
@@ -34,7 +64,28 @@ const ApplicationFile = ({ documents }: Data) => {
               </div>
             ))}
           </div>
+          {documents.length > 6 && (
+            <div className="govuk-grid-row grid-row-extra-bottom-margin">
+              <div className="govuk-grid-column-full">
+                <p className="govuk-hint">
+                  Showing 6 of {documents.length} documents
+                </p>
+                <a
+                  href={"#"}
+                  role="button"
+                  className="govuk-button govuk-button--primary"
+                  data-module="govuk-button"
+                >
+                  Show all {documents.length} documents
+                </a>
+              </div>
+            </div>
+          )}
         </>
+      ) : (
+        <p className="govuk-hint">
+          <em>No documents have been published at this time.</em>
+        </p>
       )}
     </>
   );
