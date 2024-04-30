0a40fe55d1b6848621e2a2a61c10c81beeb2e12f
gabimartini <gabimartinii@hotmail.com>
2024-04-30 17:18:37 +0100

    handle error

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,16 +7,20 @@ export async function getApplicationsByCouncil(
 ) {
   const apiKey = council + "_api_key";
   const councilApi = "NEXT_PUBLIC_BOPS_API_" + council.toUpperCase();
-  const data = await fetch(
-    `${process.env[councilApi]}planning_applications?page=${page}&maxresults=${resultsPerPage}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env[apiKey]}`,
+  if (process.env[councilApi] == undefined) {
+    return { status: 404, message: "Council not registered" };
+  } else {
+    const data = await fetch(
+      `${process.env[councilApi]}planning_applications?page=${page}&maxresults=${resultsPerPage}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env[apiKey]}`,
+        },
       },
-    },
-  );
-  return data.json();
+    );
+    return data.json();
+  }
 }
 
 // This function search by int ID, in the future it should search by reference number
