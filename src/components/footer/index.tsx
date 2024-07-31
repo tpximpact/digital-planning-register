6ca0677191a6b01f298e5d53e327b3b36385a4da
Maddie Williams <107464669+madz-lw@users.noreply.github.com>
2024-07-31 17:06:41 +0100

    Remove comments

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,19 +14,6 @@ const Footer = () => {
             />
           </div>
           <div className="govuk-footer__meta-item govuk-footer__meta-item--grow">
-            <h2 className="govuk-visually-hidden">Support links</h2>
-            {/* <ul className="govuk-footer__inline-list">
-              <li className="govuk-footer__inline-list-item">
-                <a className="govuk-footer__link" href="#">
-                  Privacy policy
-                </a>
-              </li>
-              <li className="govuk-footer__inline-list-item">
-                <a className="govuk-footer__link" href="#">
-                  Cookie policy
-                </a>
-              </li>
-            </ul> */}
             <svg
               aria-hidden="true"
               focusable="false"
