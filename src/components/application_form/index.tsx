312dfa2d0b5083a6c554f48135421d7227048417
gabimartini <gabimartinii@hotmail.com>
2024-08-09 11:06:47 -0300

    refactor formatting

--- src/components/application_form/index.tsx
+++ src/components/application_form/index.tsx
@@ -1,6 +1,8 @@
 import { DprDocument } from "@/types";
 import { capitaliseWord } from "../../../util/capitaliseWord";
 import { BopsV2PlanningApplicationsSubmission } from "@/types/api/bops";
+import { validateSecondTopic } from "../../../util/applicationForm";
+import Map from "../map";
 
 /**
  * This generates a fake document - currently using the BopsNonStandardDocument type
@@ -42,12 +44,39 @@ const isObject = (value: any): boolean => {
   return value && typeof value === "object" && !Array.isArray(value);
 };
 
-const Row = ({ description, value }: RowProps) => {
+const Row = ({ description, value, image }: RowProps) => {
+  // console.log({ value }, { description }, "val");
+  const boundaryGeojson = value;
+
+  let geometryType;
+  let coordinates;
+
+  if (boundaryGeojson?.type === "Feature") {
+    geometryType = boundaryGeojson.geometry?.type;
+    coordinates = boundaryGeojson.geometry?.coordinates;
+  } else if (boundaryGeojson?.type === "FeatureCollection") {
+    const features = boundaryGeojson.features;
+    if (features && features.length > 0) {
+      geometryType = features[0].geometry?.type;
+      coordinates = features[0].geometry?.coordinates;
+    }
+  }
+
   return (
     <div className="govuk-summary-list__row">
-      <dt className="govuk-summary-list__key">{capitaliseWord(description)}</dt>
+      <dt className="govuk-summary-list__key">{description}</dt>
       <dd className="govuk-summary-list__value">
-        {isObject(value) ? (
+        {image && geometryType && coordinates ? (
+          <Map
+            geojsonData={JSON.stringify({
+              type: "Feature",
+              geometry: {
+                type: geometryType,
+                coordinates,
+              },
+            })}
+          />
+        ) : isObject(value) ? (
           <dl className="govuk-summary-list">
             {Object.entries(value).map(([key, val], i) => (
               <Row key={i} description={key} value={val} />
@@ -77,14 +106,23 @@ const Row = ({ description, value }: RowProps) => {
   );
 };
 
-const Section = ({ title, data }: SectionProps) => {
+const Section = ({ title, data }: any) => {
   return (
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-full">
         <h3 className="govuk-heading-m">{title}</h3>
 
         <dl className="govuk-summary-list">
-          {data?.map((item, i) => <Row key={i} {...item} />)}
+          {data?.length
+            ? data?.map((item, i) => <Row key={i} {...item} />)
+            : isObject(data) &&
+              Object.entries(data).map(([key, val], i) =>
+                Array.isArray(val) ? (
+                  val.map((el, i) => <Row key={i} {...el} />)
+                ) : (
+                  <Row key={i} {...val} />
+                ),
+              )}
         </dl>
       </div>
     </div>
@@ -95,17 +133,102 @@ const ApplicationForm = ({ submission }: ApplicationFormProps) => {
   if (!submission) {
     return null;
   }
+  // validateMainTopic(submission);
+  const title = [
+    "Property",
+    "Proposal",
+    "Applicant",
+    // agent: "Applicant's agent",
+    "Application",
+    "Files",
+
+    "The Property",
+    "About you",
+    "The Project",
+    "Your application",
+    "Upload your documents",
+    "Review and Confirm",
+    "Pay and send",
+  ];
+  const arr: any = [];
+  // Object.entries(submission.data).map(([key, value]) => {
+  //   const data = Object.entries(value).map(([description, val]) =>
+  //     validateSecondTopic(description, val),
+  //   );
+  //   arr.push(...data);
+  // });
+  // console.log(submission.responses)
+  // Object.entries(submission).map(([key, value]) => {
+  //   // console.log(key)
+  //   const data =
+  //     key !== "data" &&
+  //     Object.entries(value).map(([description, val]) => ({
+  //       description,
+  //       value: typeof val === "object" ? val : String(val),
+  //     }));
+  //   arr.push(data);
+  // });
+  // console.log(arr);
+  // console.log(submission);
+  Object.entries(submission).map(([key, val], i) => {
+    // console.log(val);
+    switch (key) {
+      case "data":
+        Object.entries(val).map(([key, value], i) => {
+          const data = validateSecondTopic(key, value);
+          return data.subtopic !== undefined && arr.push(data);
+        });
+        return arr;
+      case "files":
+        Object.entries(val).map(([key, value], i) => {
+          const data = validateSecondTopic("files", value);
+          return arr.push(data);
+        });
+        return arr;
+      case "metadata":
+        Object.entries(val).map(([key, value], i) => {
+          // console.log(value, 'key')
+          // const data = validateSecondTopic("files", value);
+          // return arr.push(data);
+        });
+        return 3;
+      case "responses":
+        let cleanData: any = [];
+        Object.entries(val).map(([key, value], i) => {
+          // console.log(value, 'key')
+          const data = validateSecondTopic(value?.metadata?.sectionName, value);
+          // console.log(data, 'data')
+          if (cleanData[cleanData.length - 1]?.subtopic == data.subtopic) {
+            cleanData[cleanData.length - 1]?.value?.push(data.value[0]);
+          } else {
+            cleanData.push(data);
+          }
+          return cleanData;
+        });
+        arr.push(...cleanData);
+        return arr;
+    }
+  });
   return (
     <div className="govuk-grid-row faux-document">
       <div className="govuk-grid-column-full">
-        {Object.entries(submission.data).map(([key, value]) => {
+        {/* {console.log(arr)} */}
+        {/* {console.log(arr)} */}
+        {arr.map((el, i) => (
+          <Section title={el.subtopic} data={el.value} key={i} />
+        ))}
+        {/* {Object.entries(submission.data).map(([key, value]) => {
           const data = Object.entries(value).map(([description, val]) => ({
             description,
             value: typeof val === "object" ? val : String(val),
           }));
 
-          return <Section key={key} title={capitaliseWord(key)} data={data} />;
-        })}
+          return <Section key={key} title={capitaliseWord(key)} data={arr} />;
+        })} */}
+        {/* {title.map((el, i) => (
+          <Section title={el} data={submission} key={i} />
+        ))} */}
+        {/* <Section  title={capitaliseWord(key)} data={data} />; */}
       </div>
     </div>
   );
---
+++ util/applicationForm.ts
@@ -0,0 +1,137 @@
+export function validateSecondTopic(value: any, res: any) {
+  const subtopic: any = {
+    property: "Property",
+    proposal: "Proposal",
+    applicant: "Applicant",
+    application: "Application",
+    files: "Files",
+    "The property": "The Property",
+    "About you": "About You",
+    "The project": "The Project",
+    "Your application": "Your Application",
+    "Upload application documents": "Upload application documents",
+    "Review and confirm": "Review and Confirm",
+    "Pay and send": "Pay and send",
+  };
+  console.log(value);
+  const topic: any = {
+    property: {
+      EPC: { description: "EPC", value: res["EPC"]?.known },
+      type: { description: "Property type", value: res["type"]?.description },
+      region: { description: "Region", value: res["region"] },
+      address: [
+        { description: "Address", value: res["address"]?.singleLine },
+        {
+          description: "Coordinates (lat, long)",
+          value: `${res["address"]?.latitude}, ${res["address"]?.longitude}`,
+        },
+        {
+          description: "UPRN",
+          value: res["address"]?.uprn,
+        },
+        {
+          description: "USRN",
+          value: res["address"]?.usrn,
+        },
+      ],
+      parking: {
+        description: "Parking",
+        value: [
+          { cars: res["parking"]?.cars?.count },
+          { vans: res["parking"]?.vans?.count },
+          { buses: res["parking"]?.buses?.count },
+          { other: res["parking"]?.other?.count },
+          { cycles: res["parking"]?.cycles?.count },
+          { carClub: res["parking"]?.carClub?.count },
+          { disabled: res["parking"]?.disabled?.count },
+          { offStreet: res["parking"]?.offStreet?.residential?.count },
+          { motorcycles: res["parking"]?.motorcycles?.count },
+        ],
+      },
+      boundary: {
+        description: "Area",
+        value: `${res["boundary"]?.area?.squareMetres} m2 / ${res["boundary"]?.area?.hectares} hectares`,
+      },
+      planning: {
+        description: "Planning designation",
+        value: res["planning"]?.designations?.map((el: any) => el.description),
+      },
+      titleNumber: {
+        description: "Title numbera",
+        value: res["titleNumber"]?.known,
+      },
+      localAuthority: [
+        {
+          description: "Local authority",
+          value: res["localAuthorityDistrict"],
+        },
+        {
+          description: "Property boundary",
+          value: res["boundary"]?.site,
+          image: true,
+        },
+      ],
+    },
+    proposal: {
+      date: {
+        description: "Construction dates",
+        value: `${res["date"]?.start} to ${res["date"]?.completion}`,
+      },
+      description: { description: "Description", value: res?.description },
+      projectType: {
+        description: "Project type",
+        value: res?.projectType?.map((el) => el.description),
+      },
+      // boundary: { description: "Construction dates", value: ""}
+    },
+    applicant: {
+      type: { description: "Applicant type", value: res?.type },
+      address: {
+        description: "Applicant address",
+        value: res?.address?.sameAsSiteAddress && "Same as site address",
+      },
+      siteContact: {
+        description: "Site contact",
+        value: res?.siteContact?.role,
+      },
+    },
+    application: {
+      type: { description: "Application type", value: res?.type?.description },
+    },
+    files: file(res),
+    "The property": questions(res, value),
+    "About you": questions(res, value),
+    "The project": questions(res, value),
+    "Your application": questions(res, value),
+    "Upload application documents": questions(res, value),
+    "Review and confirm": questions(res, value),
+    "Pay and send": questions(res, value),
+  };
+
+  return { subtopic: subtopic[value], value: topic[value] };
+}
+function questions(value: any, key: any) {
+  console.log(key, "key");
+  let arr: any = [];
+  //   console.log(value.metadata?.sectionName, key)
+  if (value.metadata?.sectionName == key) {
+    arr.push({
+      description: value.question,
+      value: value.responses.map((el) => el.value),
+    });
+  }
+  //   console.log(arr, 'arr')
+  return arr;
+}
+
+function file(value: any) {
+  const filesDoc =
+    value.type &&
+    Object.entries(value?.type).map(([key, val], i) => {
+      return {
+        description: "File upload",
+        value: `${val?.description}: ${val?.value}`,
+      };
+    });
+  return filesDoc;
+}
