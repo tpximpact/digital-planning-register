1ca2c460e3edd826e4a46d246849e027ac27c657
gabimartini <gabimartinii@hotmail.com>
2024-09-05 09:37:57 +0100

    initial dsn call

---
+++ src/actions/public-application-dsn.ts
@@ -0,0 +1,12 @@
+"use server";
+
+/**
+ Get list of application that has digital site notice
+ * @param page
+ * @param resultsPerPage
+ * @param council
+ * @returns
+ */
+export async function getDSNApplication() {
+  // return application
+}
