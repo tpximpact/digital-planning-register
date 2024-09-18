e5c87d042639c18872b8b528970a97ccd5b104b2
gabimartini <gabimartinii@hotmail.com>
2024-09-18 11:16:09 +0100

    update data

--- __tests__/components/application_people.test.js
+++ __tests__/components/application_people.test.js
@@ -2,12 +2,16 @@ import "@testing-library/jest-dom";
 
 import ApplicationPeople from "@/components/application_people";
 import { screen, render } from "@testing-library/react";
+
 describe("Render ApplicationPeople", () => {
   const mockData = {
     agent_first_name: "John",
     agent_last_name: "Schmitt",
     applicant_first_name: "Rachel",
     applicant_last_name: "Green",
+    applicant_type: "Individual",
+    applicant_address: "London, LN12 5TG",
+    agent_address: "",
   };
 
   it("should render correct", async () => {
