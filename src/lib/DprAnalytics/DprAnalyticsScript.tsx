a0d025a56cf705e0bf66bbe3d273cc9df915bcda
gabimartini <gabimartinii@hotmail.com>
2024-09-30 09:25:36 +0100

    updaye analitics

--- src/lib/DprAnalytics/DprAnalyticsScript.tsx
+++ src/lib/DprAnalytics/DprAnalyticsScript.tsx
@@ -45,20 +45,24 @@ function DprAnalyticsScript(
       </Script> */}
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-4W3F40M91N"
+        src="https://www.googletagmanager.com/gtag/js?id=G-PVJ2XDYDRH"
       ></Script>
       <Script id="google-analytics">
-        {`  window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-4W3F40M91N');
+        {`    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PVJ2XDYDRH');
   `}
       </Script>
-      <Script
-        src={`https://www.googletagmanager.com/gtm.js?id=G-4W3F40M91N`}
-        id="google-tag-manager"
-        strategy="afterInteractive"
-      />
+
+      {/* <Script id="google-manage-tag">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5MB3T9HB');`}
+      </Script> */}
     </>
   );
 }
