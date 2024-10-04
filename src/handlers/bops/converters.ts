91d5580846124ddff355745f281a3f1344900450
gabimartini <gabimartinii@hotmail.com>
2024-10-04 13:36:09 +0100

    any

--- src/handlers/bops/converters.ts
+++ src/handlers/bops/converters.ts
@@ -23,7 +23,7 @@ import { formatTag } from "@/util";
 export const functionTest = (
   applicant: DprPlanningApplicationApplicant,
   privateApplication?: BopsV2PlanningApplicationDetail | null,
-): DprPlanningApplicationApplicant => {
+): any => {
   return {
     name: {
       first: "",
