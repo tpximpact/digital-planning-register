8bc69938dc987a52e45efa0f396189a90b7498ec
Han <apricot13@users.noreply.github.com>
2024-10-25 16:04:15 +0100

    summary card and list components

---
+++ src/components/govuk/SummaryList/SummaryList.tsx
@@ -0,0 +1,97 @@
+import Link from "next/link";
+import "./SummaryList.scss";
+
+export interface SummaryListProps {
+  rows: {
+    key: {
+      text: JSX.Element | string;
+    };
+    value: {
+      text: JSX.Element | string;
+    };
+    actions?: {
+      items: {
+        href: string;
+        text: string;
+        visuallyHiddenText: string;
+      }[];
+    };
+  }[];
+  cardTitle?: {
+    text: string;
+    headingLevel?: string;
+  };
+  noBorder?: boolean;
+}
+
+export const ActionLink = ({
+  action,
+  cardTitle,
+}: {
+  action: NonNullable<
+    SummaryListProps["rows"][number]["actions"]
+  >["items"][number];
+  cardTitle: SummaryListProps["cardTitle"];
+}) => {
+  return (
+    <Link className="govuk-link" href={action.href}>
+      {action.text}
+      {(action.visuallyHiddenText || cardTitle) && (
+        <span className="govuk-visually-hidden">
+          {action.visuallyHiddenText && <>{action.visuallyHiddenText}</>}
+          {cardTitle && <>{cardTitle.text}</>}
+        </span>
+      )}
+    </Link>
+  );
+};
+
+export const SummaryList = ({
+  rows,
+  cardTitle,
+  noBorder,
+}: SummaryListProps) => {
+  if (!rows) {
+    return null;
+  }
+  return (
+    <>
+      <dl
+        className={`govuk-summary-list ${noBorder ? "govuk-summary-list--no-border" : ""}`}
+      >
+        {rows.map((row, index) => (
+          <div
+            key={index}
+            className={`govuk-summary-list__row ${row.actions ? "govuk-summary-list__row--no-actions" : ""}`}
+          >
+            <dt className="govuk-summary-list__key">{row.key.text}</dt>
+            <dd className="govuk-summary-list__value">{row.value.text}</dd>
+            {row.actions && (
+              <dd className="govuk-summary-list__actions">
+                {row.actions.items.length === 1 ? (
+                  <>
+                    <ActionLink
+                      action={row.actions.items[0]}
+                      cardTitle={cardTitle}
+                    />
+                  </>
+                ) : (
+                  <ul className="govuk-summary-list__actions-list">
+                    {row.actions.items.map((action, index) => (
+                      <li
+                        key={`items-${index}`}
+                        className="govuk-summary-list__actions-list-item"
+                      >
+                        <ActionLink action={action} cardTitle={cardTitle} />
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </dd>
+            )}
+          </div>
+        ))}
+      </dl>
+    </>
+  );
+};
