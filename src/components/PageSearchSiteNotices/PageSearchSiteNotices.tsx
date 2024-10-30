6d6bac03c278816d4b1e13457d5539d1b8f9df54
Han <apricot13@users.noreply.github.com>
2024-10-30 17:11:28 +0000

    add pagination to dsn search page

--- src/components/PageSearchSiteNotices/PageSearchSiteNotices.tsx
+++ src/components/PageSearchSiteNotices/PageSearchSiteNotices.tsx
@@ -3,13 +3,18 @@ import { BackLink } from "../button";
 import { ContentNoResult } from "../ContentNoResult/ContentNoResult";
 import { AppConfig } from "@/config/types";
 import ApplicationCard from "../application_card";
-import { Pagination } from "../Pagination";
+import { Pagination } from "@/components/govuk/Pagination";
 import ApplicationMap from "../application_map";
+import { createPathFromParams } from "@/lib/navigation";
 
 export interface PageSearchSiteNoticesProps {
   appConfig: AppConfig;
   applications: DprPlanningApplication[] | undefined;
   pagination: DprPagination | undefined;
+  params?: {
+    council: string;
+    reference?: string;
+  };
   searchParams?: SearchParams;
 }
 
@@ -17,6 +22,7 @@ export const PageSearchSiteNotices = ({
   appConfig,
   applications,
   pagination,
+  params,
   searchParams,
 }: PageSearchSiteNoticesProps) => {
   if (!appConfig || !appConfig.council) {
@@ -64,14 +70,9 @@ export const PageSearchSiteNotices = ({
           </div>
           {pagination && pagination.total_pages > 1 && (
             <Pagination
-              currentPage={page - 1}
-              totalItems={
-                pagination.total_pages * appConfig.defaults.resultsPerPage
-              }
-              itemsPerPage={appConfig.defaults.resultsPerPage}
-              totalPages={pagination.total_pages}
-              baseUrl={`/${appConfig.council.slug}/digital-site-notice`}
-              queryParams={searchParams}
+              baseUrl={createPathFromParams(params)}
+              searchParams={searchParams}
+              pagination={pagination}
             />
           )}
         </>
