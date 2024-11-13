c5927141a409d7f89051585f59aa7d2e3fe9e82a
gabimartini <gabimartinii@hotmail.com>
2024-11-13 10:21:00 +0000

    add content side bar info

--- src/components/ApplicationDetails/ApplicationDetails.tsx
+++ src/components/ApplicationDetails/ApplicationDetails.tsx
@@ -6,6 +6,10 @@ import { ApplicationDetailsInformation } from "../ApplicationDetailsInformation"
 import { DocumentsList } from "@/components/DocumentsList";
 import { PageWrapper } from "../PageWrapper";
 import { ContentError } from "../ContentError";
+import { ContentSidebar } from "../ContentSidebar";
+import "./ApplicationDetails.scss";
+import Link from "next/link";
+import { contentSideBarChildren } from "../ContentSidebar/contentSideBarHelp";
 
 export interface ApplicationDetailsProps {
   reference: string;
@@ -38,46 +42,80 @@ export const ApplicationDetails = ({
       )}
       {/* <ApplicationLocation /> */}
       {/* <ApplicationMoreDetails {...application} /> */}
-      <DocumentsList
-        councilSlug={appConfig?.council?.slug}
-        reference={reference}
-        showMoreButton={true}
-        documents={documents?.slice(0, 3) ?? null}
-        totalDocuments={documents?.length ?? 0}
-      />
-      <ApplicationPeople
-        applicant_first_name={application.application.applicant_first_name}
-        applicant_last_name={application.application.applicant_last_name}
-        agent_first_name={application.application.agent_first_name}
-        agent_last_name={application.application.agent_last_name}
-      />
-      {appConfig.council?.specialistComments && (
-        <CommentsList
-          councilSlug={appConfig?.council?.slug}
-          reference={reference}
-          type="specialist"
-          pagination={{
-            page: 1,
-            results: 3,
-          }}
-          showMoreButton={true}
-          comments={application.application.consultation.consulteeComments}
-        />
-      )}
-      {appConfig.council?.publicComments && (
-        <CommentsList
-          councilSlug={appConfig?.council?.slug}
-          reference={reference}
-          type="public"
-          pagination={{
-            page: 1,
-            results: 3,
-          }}
-          showMoreButton={true}
-          comments={application.application.consultation.publishedComments}
-        />
-      )}
-      {/* <ApplicationConstraints /> */}
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-one-third-from-desktop contents-bar-list">
+          <ContentSidebar
+            content={[
+              {
+                key: "",
+                title: "",
+                children: contentSideBarChildren,
+              },
+            ]}
+          />
+          {application?.application?.status !== "determined" && (
+            <div className="govuk-grid-row extra-top-margin">
+              <div className="govuk-grid-column-full">
+                <Link
+                  href={`/${appConfig.council.slug}/${reference}/submit-comment`}
+                  className="govuk-button govuk-button--primary"
+                  data-module="govuk-button"
+                >
+                  Comment on this application
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="govuk-grid-column-two-thirds-from-desktop">
+          <h2 className="govuk-heading-l" id="Description">
+            Description
+          </h2>
+          <p className="govuk-body" id="application-description">
+            {application?.proposal?.description}
+          </p>
+          <DocumentsList
+            councilSlug={appConfig?.council?.slug}
+            reference={reference}
+            showMoreButton={true}
+            documents={documents?.slice(0, 3) ?? null}
+            totalDocuments={documents?.length ?? 0}
+          />
+          <ApplicationPeople
+            applicant_first_name={application.application.applicant_first_name}
+            applicant_last_name={application.application.applicant_last_name}
+            agent_first_name={application.application.agent_first_name}
+            agent_last_name={application.application.agent_last_name}
+          />
+          {appConfig.council?.specialistComments && (
+            <CommentsList
+              councilSlug={appConfig?.council?.slug}
+              reference={reference}
+              type="specialist"
+              pagination={{
+                page: 1,
+                results: 3,
+              }}
+              showMoreButton={true}
+              comments={application.application.consultation.consulteeComments}
+            />
+          )}
+          {appConfig.council?.publicComments && (
+            <CommentsList
+              councilSlug={appConfig?.council?.slug}
+              reference={reference}
+              type="public"
+              pagination={{
+                page: 1,
+                results: 3,
+              }}
+              showMoreButton={true}
+              comments={application.application.consultation.publishedComments}
+            />
+          )}
+          {/* <ApplicationConstraints /> */}
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/ContentSidebar/contentSideBarHelp.tsx
@@ -0,0 +1,30 @@
+export const contentSideBarChildren = [
+  {
+    key: "Key information",
+    title: "Key information",
+  },
+  {
+    key: "Progress",
+    title: "Progress",
+  },
+  {
+    key: "Description",
+    title: "Description",
+  },
+  {
+    key: "Documents",
+    title: "Documents",
+  },
+  {
+    key: "People",
+    title: "People",
+  },
+  {
+    key: "Specialist comments",
+    title: "Specialist comments",
+  },
+  {
+    key: "Public comments",
+    title: "Public comments",
+  },
+];
