9745709b7d9858a0897889bce8866df8a3990b21
gabimartini <gabimartinii@hotmail.com>
2024-11-21 08:36:48 +0000

    add style

--- src/components/ApplicationProgressInfo/ApplicationProgressInfo.tsx
+++ src/components/ApplicationProgressInfo/ApplicationProgressInfo.tsx
@@ -2,12 +2,24 @@
 import { useState } from "react";
 import "./ApplicationProgressInfo.scss";
 
-export const ApplicationProgressInfo = ({ applicationHistory }: any) => {
+interface History {
+  info: string;
+  date: string;
+  details: string;
+}
+
+interface ApplicationHistory {
+  applicationHistory: History[];
+}
+
+export const ApplicationProgressInfo = ({
+  applicationHistory,
+}: ApplicationHistory) => {
   const [isInfoOpen, setIsInfoOpen] = useState<boolean>(false);
   const [infoId, setInfoId] = useState<string>();
   return (
     <div
-      className="govuk-accordion"
+      className="accordion"
       data-module="govuk-accordion"
       id="accordion-default"
     >
@@ -16,58 +28,60 @@ export const ApplicationProgressInfo = ({ applicationHistory }: any) => {
       </h2>
 
       <div
-        className="govuk-accordion timeline"
+        className="accordion timeline"
         data-module="govuk-accordion"
         id="accordion-default"
       >
         {applicationHistory.length > 0 &&
-          applicationHistory?.map((history: any, index: any) => (
+          applicationHistory?.map((history: History, index: number) => (
             <div
-              className={`govuk-accordion__section ${applicationHistory[applicationHistory.length - 1] == history && "timeline-end"}`}
+              className={`accordion__section ${applicationHistory[applicationHistory.length - 1] == history && "timeline-end"}`}
               key={history?.info}
             >
-              <div className="govuk-accordion__section-header">
-                <h2 className="govuk-accordion__section-heading">
-                  <button
-                    onClick={() => {
-                      if (infoId !== history?.info) {
-                        setIsInfoOpen(true);
-                        setInfoId(history?.info);
-                      } else {
-                        setIsInfoOpen(!isInfoOpen);
-                        setInfoId(history?.info);
-                      }
-                    }}
-                    className="govuk-accordion__section-button"
-                  >
-                    <span
-                      className="accordion__section-heading-text"
-                      id={`accordion-default-heading-${index}`}
+              <div>
+                <div className="accordion__section-header">
+                  <h2 className="accordion__section-heading">
+                    <button
+                      onClick={() => {
+                        if (infoId !== history?.info) {
+                          setIsInfoOpen(true);
+                          setInfoId(history?.info);
+                        } else {
+                          setIsInfoOpen(!isInfoOpen);
+                          setInfoId(history?.info);
+                        }
+                      }}
+                      className="govuk-accordion__section-button accordion__section-button"
                     >
-                      <span className="accordion__section-heading-text-focus">
-                        {history?.info}{" "}
-                      </span>
-                      <span className="timeline-date">{history?.date}</span>
-                    </span>
-                    <span className="govuk-accordion__section-toggle">
                       <span
-                        className={`govuk-accordion-nav__chevron ${(!isInfoOpen || infoId != history?.info) && "govuk-accordion-nav__chevron--down"}`}
-                      ></span>
-                      {isInfoOpen && infoId == history?.info
-                        ? "Hide more info"
-                        : "Show more info"}
-                    </span>
-                  </button>
-                </h2>
-              </div>
-              {isInfoOpen && infoId == history?.info && (
-                <div
-                  id={`accordion-default-content-${index}`}
-                  className="govuk-accordion__section-content"
-                >
-                  <p className="govuk-body">{history?.details}</p>
+                        className="accordion__section-heading-text"
+                        id={`accordion-default-heading-${index}`}
+                      >
+                        <span className="accordion__section-heading-text-focus">
+                          {history?.info}{" "}
+                        </span>
+                        <span className="timeline-date">{history?.date}</span>
+                      </span>
+                      <span className="accordion__section-toggle">
+                        <span
+                          className={`accordion-nav__chevron ${(!isInfoOpen || infoId != history?.info) && "accordion-nav__chevron--down"}`}
+                        ></span>
+                        {isInfoOpen && infoId == history?.info
+                          ? "Hide more info"
+                          : "Show more info"}
+                      </span>
+                    </button>
+                  </h2>
                 </div>
-              )}
+                {isInfoOpen && infoId == history?.info && (
+                  <div
+                    id={`accordion-default-content-${index}`}
+                    className="accordion__section-content govuk-accordion__section-content"
+                  >
+                    <p className="govuk-body">{history?.details}</p>
+                  </div>
+                )}
+              </div>
             </div>
           ))}
       </div>
