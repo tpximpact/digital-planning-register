837c595991ad443e830def9c81e779c743926186
gabimartini <gabimartinii@hotmail.com>
2025-05-19 08:22:14 +0100

    fix skeleton

--- src/components/ApplicationCommentsSummary/ApplicationCommentsSummarySkeleton.tsx
+++ src/components/ApplicationCommentsSummary/ApplicationCommentsSummarySkeleton.tsx
@@ -4,16 +4,10 @@ import "./ApplicationCommentsSummary.scss";
 export const SummarySkeleton = (type: { type: DprCommentTypes }) => {
   return (
     <div id={`${type}-comments-summary`}>
-      <div className="dpr-comment-summary">
-        <div className="dpr-comment-summary__header">
-          <div>
-            <div
-              className="dpr-comment-summary__skeleton--item "
-              data-testid="summary-skeleton"
-            ></div>
-          </div>
-        </div>
-      </div>
+      <div
+        className="dpr-comment-summary__skeleton "
+        data-testid="summary-skeleton"
+      ></div>
     </div>
   );
 };
