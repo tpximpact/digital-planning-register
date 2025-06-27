f14435a35cdf8b5a8515296785be33290c521db4
Han <apricot13@users.noreply.github.com>
2025-06-27 17:51:21 +0100

    DSNPI-1265 Constraints trial run

--- __tests__/components/ApplicationConstraints.test.js
+++
@@ -1,9 +0,0 @@
-import { ApplicationConstraints } from "@/components/ApplicationConstraints";
-import { screen, render } from "@testing-library/react";
-describe("Render ApplicationConstraints", () => {
-  it("should render correct", () => {
-    render(<ApplicationConstraints />);
-
-    expect(screen.getByRole("heading", { name: "Constraints" }));
-  });
-});
